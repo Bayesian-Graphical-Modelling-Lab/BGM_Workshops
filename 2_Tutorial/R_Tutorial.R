fa633786f23f6671147c41640a009d449b7a882d
Nikola Sekulovski <nikolasekulovski7@gmail.com>
2024-09-26 18:30:45 +0200

    update

--- 2_Tutorial/R_Tutorial.R
+++ 2_Tutorial/R_Tutorial.R
@@ -8,7 +8,7 @@
 # Set working directory
 
 # Load background functions
-install.packages("easybgm")
+# install.packages("easybgm")
 # remotes::install_github("KarolineHuth/easybgm", ref = "developer")
 
 library(easybgm)
@@ -24,6 +24,7 @@ data <- read.csv("exampledata.csv")[, -1]
 # =========================
 
 # 1. Fit the model
+?easybgm
 res <- easybgm(data = data,          #(M) n*p matrix of responses
                type = "continuous",  #(M) type of data
                package = "BGGM",     #(O) package to use
@@ -32,7 +33,9 @@ res <- easybgm(data = data,          #(M) n*p matrix of responses
                centrality = FALSE,   #(O) Should centrality be computed
                progress = TRUE)      #(O) Should the progress bar be plotted
 
-summary(res, evidence_thresh = 3)
+
+# summary of the results 
+summary(res, evidence_thresh = 10)
 
 # -----------------------------------------------------------------------------------------------------------------
 
@@ -46,19 +49,22 @@ plot_edgeevidence(res, evidence_thresh = 10)
 
 # To help with the interpretation of networks, we'll split it in two
 par(mfrow = c(1, 2))
-plot_edgeevidence(res, edge.width = 3, split = T, legend = F, evidence_thresh = 10)
+plot_edgeevidence(res, edge.width = 3, 
+                  split = T, legend = F, 
+                  evidence_thresh = 10)
 
 # -----------------------------------------------------------------------------------------------------------------
 # b. Plot network model (Estimation)
 par(mfrow = c(1, 1))
-plot_network(res, exc_prob = 0.5, dashed = T)
+plot_network(res, exc_prob = 0.5)
 
 # customizing the plot
 Names_data <- colnames(data)
 groups_data <- c(rep("DASS", 3), rep("Personality", 10))
-plot_network(res, exc_prob = .999, layout = "spring", nodeNames = Names_data, groups = groups_data, 
-             color= c("#fbb20a","#E59866"), theme = "Borkulo", dashed = T
-             )
+plot_network(res, exc_prob = 0.5, layout = "spring", 
+             nodeNames = Names_data, groups = groups_data, 
+             color= c("#fbb20a","#E59866"), 
+             theme = "Borkulo", dashed = T)
 
 # -----------------------------------------------------------------------------------------------------------------
 
@@ -76,21 +82,40 @@ res <- easybgm(data = data,          #(M) n*p matrix of responses
 plot_parameterHDI(res)
 
 # -----------------------------------------------------------------------------------------------------------------
-# d. Plot strength centrality (will not discussed)
+# d. Plot strength centrality
 
 #plot_centrality(res)
 
 # =========================
 # 3. Priors 
 # =========================
+# Note that the help file says that bgms has two options for the 
+# interaction prior; this is no longer the case, only the Cauchy prior 
+#(with an adjustable scale parameter) is available. Additionally,
+# the stochastic block as a prior on the network structure
+# (i.e., edge prior) is available for use however, a paper
+# explaining the method is still in preparation.
 
-?bgm
-res <- easybgm(data = as.matrix(data[1:300, 5:10]),          #(M) n*p matrix of responses
+# If you use the Beta-Bernoulli prior, please note that
+# there is an issue when calculating the inclusion Bayes factor
+# due to an issue in the calculation of the prior odds
+# in this case, simply use the posterior inclusion probability
+# as a measure of evidence for the inclusion of an edge. Or
+# posterior inclusion probability/(1-posterior inclusion probability)
+# in case you use beta_bernoulli_alpha = 1 and beta_bernoulli_beta = 1
+
+res <- easybgm(data = Wenchuan[, 1:5]),#(M) n*p matrix of responses
                type = "ordinal",  #(M) type of data
                package = "bgms",  #(O) type of sampling algorithm
-               iter = 3e3,           #(O) no. iterations sampler 1e5
-               save = TRUE,          #(O) Should samples be stored
+               iter = 3e3,           #(O) no. iterations (for illustrative purposes)
+               save = FALSE,          #(O) Should samples be stored
                centrality = TRUE,   #(O) Should centrality be computed
                progress = TRUE,      #(O) Should the progress bar be plotted
                inclusion_probability = 0.7)
+
+par(mfrow = c(1, 2))
+plot_edgeevidence(res, edge.width = 3, split = F, 
+                  legend = F, evidence_thresh = 10)
+summary(res)
+
 
